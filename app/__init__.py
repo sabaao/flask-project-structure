f86972f298e134e6e322798e1a80eb45fdbac3b2
Charles <charles@cathayholdings.com.tw>
2021-07-04 18:36:37 +0800

    Add user api and mongo setting

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+
+mongo = PyMongo()
 
 def create_app():
     app = Flask(__name__)
+    # app.config.update(
+    #     MONGO_HOST='localhost',
+    #     MONGO_PORT=27017,
+    #     #MONGO_USERNAME='bjhee',
+    #     #MONGO_PASSWORD='111111',
+    #     MONGO_DBNAME='demo'
+    # )
+
+    app.config["MONGO_URI"] = "mongodb://localhost:27017/demo"
+    mongo.init_app(app)
 
     from app.users.controller import mod as users_mod
 
--- app/users/controller.py
+++ app/users/controller.py
@@ -1,7 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from app import mongo
 
 mod = Blueprint('users', __name__, url_prefix='/users')
 
 @mod.route('/',methods=['GET'])
 def index():
     return 'hello world'
+
+@mod.route('/',methods=['POST'])
+def add_users():
+    content = request.get_json()
+    print(content)
+    mongo.db.users.insert_one(content)
+    return {"success":True}
